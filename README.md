fe4d503d6f56a02a4bae2c15f4567d74a4b4dd89
EC2 Default User <ec2-user@ip-172-31-32-212.eu-west-3.compute.internal>
2019-11-25 16:09:57 +0000

    readme

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 ## Installation
 Backend: conda should be installed first, then in the __back__ folder create the env with:
 > conda env create -f 'env/gpu.yml'
+
 or
 > conda env create -f 'env/cpu.yml'
+
 if you can't use tensorflow-gpu and CUDA driver (MacOS X for example)
 
-Frontend: Node.js should be installed first, then in the __front_ folder run:
+Frontend: Node.js should be installed first, then in the __front__ folder run:
 > npm install
 
 ## Starting the app
--- back/api.py
+++ back/api.py
@@ -11,7 +11,7 @@ from rq import Queue
 #Configuration and Ping route
 
 UPLOAD_FOLDER = 'raw_data'
-OUTPUT_FOLDER = 'output'
+OUTPUT_FOLDER = '../front/public/output'
 ALLOWED_EXTENSIONS = {'mp3'}
 
 def allowed_file(filename):
@@ -29,14 +29,18 @@ def ping_pong():
     print(request)
     return jsonify('pong!')
 
-#Get stems
+#Queue the split process and respond to the request
 def split_audio(filename, stem):
     song_folder = filename
-    output_dir = Path(OUTPUT_FOLDER) / song_folder
+    output_dir = Path(OUTPUT_FOLDER)
     #split.split(str(Path(app.config['UPLOAD_FOLDER']) / filename), str(output_dir), int(stem))
     q.enqueue(split.split, str(Path(app.config['UPLOAD_FOLDER']) / filename), str(output_dir), int(stem))
     if stem == 2:
         output_files = {"voice" : song_folder + '/vocals.wav', "accompaniment": song_folder + '/accompaniment.wav'}
+    elif stem == 4:
+        output_files = {"voice" : song_folder + '/vocals.wav', "bass" : song_folder + '/bass.mp3', "drums": song_folder + '/drums.wav', "other": song_folder + '/other.wav'}
+    elif stem == 5:
+        output_files = {"voice" : song_folder + '/vocals.wav', "bass" : song_folder + '/bass.mp3', "drums": song_folder + '/drums.wav', "piano": song_folder + '/piano.wav', "other": song_folder + '/other.wav'}
     return output_files
 
 #Upload a file and convert it
@@ -49,11 +53,16 @@ def save_file():
         file = request.files['file']
         if file.filename == '':
             return 'No filename'
+        if 'stem' not in request.form:
+            return 'Please specify a number of stem in your query'
+        stem = int(request.form['stem'])
+        if stem != 2 and stem != 4 and stem != 5:
+           return 'Wrong stem value'
         if file and allowed_file(file.filename) and request.form['stem']:
             filename = secure_filename(file.filename)
             file_path = Path(app.config['UPLOAD_FOLDER']) / filename
             file.save(str(file_path))
-            stem = int(request.form['stem'])
+            print (filename)
             ret = split_audio(filename, stem)
             return jsonify(ret)
         else:
